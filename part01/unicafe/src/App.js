8a442a96054812b785aec7ee055f536bf29d8e31
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-01 20:38:04 +0300

    Added total reviews, average and positive stats

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -2,13 +2,16 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
-const Stat = (props) => <div>{props.text} {props.count}</div>
+const Stat = (props) => <div>{props.text} {props.count} {props.unit}</div>
 
 const App = () => {
   // Each button will use its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const score = good - bad
+  const total = good + neutral + bad
 
   const increment = (value, setter) => () => setter(value + 1)
 
@@ -22,6 +25,9 @@ const App = () => {
       <Stat text="good" count={good} />
       <Stat text="neutral" count={neutral} />
       <Stat text="bad" count={bad} />
+      <Stat text="all" count={total} />
+      <Stat text="average" count={score/total} />
+      <Stat text="positive" count={(good/total)*100} unit="%" />
     </div>
   )
 }
