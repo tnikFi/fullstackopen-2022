aed69af5ff510da4e135dd8cec3e980a381dc357
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-01 21:29:43 +0300

    Added anecdote voting

--- part01/anecdotes/src/App.js
+++ part01/anecdotes/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const VoteCounter = ({selected, votes}) => {
+  const voteCount = votes[selected] ? votes[selected] : 0
+  return (
+    // Return 'has 1 vote' if voteCount is 1, else 'has n votes' 
+    <p>Has {voteCount} {(voteCount === 1) ? 'vote' : 'votes'}</p>
+  )
+} 
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -10,16 +18,27 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
+  
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})
 
   const selectRandomAnecdote = () => {
     setSelected(Math.round(Math.random() * (anecdotes.length - 1)))
   }
 
+  const voteForAnecdote = () => {
+    // Using a ternary operator here because every vote count is undefined
+    // by default and therefore performing math on it would return NaN
+    const votesCopy = {...votes}
+    votesCopy[selected] ? votesCopy[selected] ++ : votesCopy[selected] = 1
+    setVotes(votesCopy)
+  }
+
   return (
     <div>
-      {anecdotes[selected]}<br/> 
+      {anecdotes[selected]}
+      <VoteCounter selected={selected} votes={votes} />
+      <button onClick={voteForAnecdote}>vote</button>
       <button onClick={selectRandomAnecdote}>next anecdote</button>
     </div>
   )
