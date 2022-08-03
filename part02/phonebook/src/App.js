485e2602f47ffb460b998e93660b1c335b10f8d6
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-03 19:36:44 +0300

    Initial state is now fetched from the server

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
@@ -27,14 +28,19 @@ const NewContactForm = ({persons, newContact, newContactHandler, nameHandler, nu
 </>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newContact, setNewContact] = useState({ name: '', number: '' })
   const [filter, setFilter] = useState('')
+
+  // Get the initial contact info from the server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  }, [])
 
   const nameInputHandler = e => {
     e.preventDefault()
