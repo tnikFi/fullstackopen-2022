f4081e91c5a6536d0f4b182d87552847d61e03b0
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-03 16:47:47 +0300

    Course parts list now supports any amount of parts

--- part02/courseinfo/src/App.js
+++ part02/courseinfo/src/App.js
@@ -2,42 +2,48 @@ const Header = (props) => <h1>{props.course.name}</h1>;
 
 const Part = props => <p>{props.name} {props.exercises}</p>;
 
-const Content = (props) => {
+const Content = ({course}) => {
   return (
     <>
-      <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-      <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-      <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      {course.parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id}/>)}
     </>
   )
 }
 
 const Total = props => <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>;
 
+const Course = ({course}) => <>
+  <Header course={course}/>
+  <Content course={course}/>
+  <Total course={course}/>
+</>
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
-    parts: [ 
+    id: 1,
+    parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course} />
+      <Course course={course} />
     </div>
   )
 }
