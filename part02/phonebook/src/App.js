e10515d44f36e6384a4e913528095f0d5842b31f
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-03 18:20:27 +0300

    Adding names to the list is now possible

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,24 +1,37 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => <p>{person.name}</p>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const nameInputHandler = (e) => {
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+
+  const newNameHandler = e => {
+    e.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={newNameHandler}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={nameInputHandler} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <Person person={person} key={person.name} />)}
     </div>
   )
 
