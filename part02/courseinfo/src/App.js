dff7d7911eaaecca078937d496ca44c156607f71
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-03 17:01:53 +0300

    Moved Course component to its own module

--- part02/courseinfo/src/App.js
+++ part02/courseinfo/src/App.js
@@ -1,22 +1,4 @@
-const Header = (props) => <h2>{props.course.name}</h2>;
-
-const Part = props => <p>{props.name} {props.exercises}</p>;
-
-const Content = ({course}) => {
-  return (
-    <>
-      {course.parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id}/>)}
-    </>
-  )
-}
-
-const Total = ({course}) => <p><strong>Number of exercises {course.parts.reduce((previousSum, current) => previousSum + current.exercises, 0)}</strong></p>;
-
-const Course = ({course}) => <>
-  <Header course={course}/>
-  <Content course={course}/>
-  <Total course={course}/>
-</>
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
