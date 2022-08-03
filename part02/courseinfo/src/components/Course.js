71f4d4887867824015bdc43eef0dc1b10b918c7f
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-03 17:03:59 +0300

    Minor code refactor

--- part02/courseinfo/src/components/Course.js
+++ part02/courseinfo/src/components/Course.js
@@ -1,16 +1,16 @@
-const Header = (props) => <h2>{props.course.name}</h2>;
+const Header = (props) => <h2>{props.course.name}</h2>
 
-const Part = props => <p>{props.name} {props.exercises}</p>;
+const Part = props => <p>{props.name} {props.exercises}</p>
 
-const Content = ({course}) => {
-  return (
-    <>
-      {course.parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id}/>)}
-    </>
-  )
-}
+const Content = ({course}) => <>
+  {course.parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id}/>)}
+</>
 
-const Total = ({course}) => <p><strong>Number of exercises {course.parts.reduce((previousSum, current) => previousSum + current.exercises, 0)}</strong></p>;
+const Total = ({course}) => <p>
+  <strong>
+    Number of exercises {course.parts.reduce((previousSum, current) => previousSum + current.exercises, 0)}
+  </strong>
+</p>
 
 const Course = ({course}) => <>
   <Header course={course}/>
