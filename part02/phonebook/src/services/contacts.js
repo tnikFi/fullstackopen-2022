c03c7165c33c497b4d9356aeb5209bd9c7700c52
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-04 17:53:16 +0300

    Production build

--- part02/phonebook/src/services/contacts.js
+++ part02/phonebook/src/services/contacts.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3001/api'
+const BASE_URL = '/api/persons'
 
 const getAll = () => {
-    const request = axios.get(`${BASE_URL}/persons`)
+    const request = axios.get(`${BASE_URL}`)
     return request.then(response => response.data)
 }
 
 const create = object => {
-    const request = axios.post(`${BASE_URL}/persons`, object)
+    const request = axios.post(`${BASE_URL}`, object)
     return request.then(response => response.data)
 }
 
 const remove = id => {
-    const request = axios.delete(`${BASE_URL}/persons/${id}`)
+    const request = axios.delete(`${BASE_URL}/${id}`)
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${BASE_URL}/persons/${id}`, newObject)
+    const request = axios.put(`${BASE_URL}/${id}`, newObject)
     return request.then(response => response.data)
 }
 
